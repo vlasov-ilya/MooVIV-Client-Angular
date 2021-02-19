d1853fdeaf641000e1be76fbe164893659b53d26
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-02-19 11:30:09 -0500

    Movie Cards back
    Data fixed.
    button order changed data fixed.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
   logoutUser(): void {
     localStorage.clear();
   }
